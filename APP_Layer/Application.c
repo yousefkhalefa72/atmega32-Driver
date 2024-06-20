a5968c5e273f2483297e2bae9c7ff3c207b30855
yousef khalefa <158314984+yousefkhalefa72@users.noreply.github.com>
2024-06-20 15:01:52 +0300

    Add files via upload

---
+++ APP_Layer/Application.c
@@ -0,0 +1,24 @@
+/* 
+ * File:   Application.c
+ * Author: Yousef.Khalefa
+ * Created on March 8, 2024, 7:49 PM
+ */
+
+#include "Application.h"
+
+
+int main(void)
+{    
+    Application_Intialize();
+
+	while(1)
+	{
+        
+	}
+    return E_OK;
+}
+
+void Application_Intialize(void)
+{
+    
+}
---
+++ APP_Layer/Application.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   Application.h
+ * Author: Yousef.Khalefa
+ * Created on March 8, 2024, 7:49 PM
+ */
+
+#ifndef APPLICATION_H
+#define	APPLICATION_H
+
+/* Section : Includes */
+#include "../HAL_Layer/HAL_Layer.h"
+#include "../MCAL_Layer/MCAL_Layer.h"
+
+/* Section: Macro Declarations */
+
+
+/* Section: Macro Functions Declarations */
+
+/* Section: Data Type Declarations */
+
+/* Section: Function Declarations */
+void Application_Intialize(void);
+
+
+#endif	/* APPLICATION_H */
+
